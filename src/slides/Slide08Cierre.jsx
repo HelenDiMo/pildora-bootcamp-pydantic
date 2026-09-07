a69e0c8cdf2047f88c703971daad0acd30844a80
HelenDiMo <elena.diazmoreno@gmail.com>
2026-09-07 21:26:14 +0200

    fix: corregir la ruta del enlace de descarga en Slide08Cierre.jsx

--- src/slides/Slide08Cierre.jsx
+++ src/slides/Slide08Cierre.jsx
@@ -21,7 +21,7 @@ export default function Slide08Cierre() {
       </div>
       <a
         className="download-link"
-        href="public\masterclass_pydantic_estudiantes_TODO.ipynb"
+        href="\public\masterclass_pydantic_estudiantes_TODO.ipynb"
         download
       >
         📥 Descargar el notebook del reto (.ipynb)
